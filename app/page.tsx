4eee4b0c87b407d0baec9eabd218253bfea6ecbd
osmanramadan <o@o.com>
2025-03-15 22:39:34 +0200

    fix-globalcss

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <Splashbtn/>
           <div className="text-center mx-3 text-3xl md:mx-2 md:text-5xl mt-5  text-[#3CB7EF]   font-bold bg-gradient-to-r from-[#40bfff] via-[#66C6F3] to-[#00ccff] bg-clip-text text-transparent">A CRM dashboard for engineering teams</div>
           
-          <div className="mt-8 mx-4  md:mx-2 text-[#94A3B8] text-center text-bold md:text-xl text-[16px] ">
+          <div className="mt-8 mx-4 px-4  md:mx-2 text-[#94A3B8] text-center text-bold md:text-xl text-[16px] ">
             Boost engineering team’s productivity with Ocean CRM dashboard that streamlines<br></br>project management, collaboration, and data-driven decision-making.
       </div>
       <Splashbtns/>
