9e4ff51dc6c9282dbed5a4ca2ea974c019ea9170
osmanramadan <o@o.com>
2025-03-15 23:03:57 +0200

    '.'

--- compenents/splashbtns.tsx
+++ compenents/splashbtns.tsx
@@ -5,12 +5,12 @@ export default function Splashbtns() {
   return (
     <div className="flex space-x-4 flex items-center justify-center mt-7 px-4">
       {/* Primary Button */}
-      <button className="w-45 h-11 !bg-[#0284C7] hover:bg-[#0EA5E9] text-[18px] text-xl text-white font-medium py-1 px-1 rounded-md transition-all duration-300">
+      <button className="w-45 h-11 !bg-[#0284C7] hover:bg-[#0EA5E9] text-[18px] md:text-xl text-white font-medium py-1 px-1 rounded-md transition-all duration-300">
         Get Started <span className="!ml-1 font-bold  !bg-[#0284C7]">→</span>
       </button>
 
       {/* Secondary Button */}
-      <button className="w-30 md:w-38  h-11  text-white py-2 px-3 rounded-md border-[0.1px] border-[#1E293B] hover:bg-[#1E293B] transition-all duration-300">
+      <button className="w-30 md:w-38  h-11 md:text-xl text-white py-2 px-3 rounded-md border-[0.1px] border-[#1E293B] hover:bg-[#1E293B] transition-all duration-300">
         View Pricing
       </button>
     </div>
