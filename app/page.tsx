67d5fd57ecba6680b493bd6b1da3ba2ba619f77a
osmanramadan <o@o.com>
2025-03-16 00:40:18 +0200

    'add-img'

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 import Splashbtn from "@/compenents/splashbtn";
 import Splashbtns from "@/compenents/splashbtns";
 
+
+
 export default function Home() {
+
   return (
     <div className="splash text-white flex items-center justify-center">
+
       <div className="mt-24">
-          <Splashbtn/>
-          <div className="text-center mx-3 text-3xl md:mx-2 md:text-5xl mt-5  text-[#3CB7EF]   font-bold bg-gradient-to-r from-[#40bfff] via-[#66C6F3] to-[#00ccff] bg-clip-text text-transparent">A CRM dashboard for engineering teams</div>
+
+      <Splashbtn/>
+
+          <div className="text-center  mx-3 md:mx-2 text-3xl md:text-5xl mt-5 text-[#3CB7EF] font-bold bg-gradient-to-r from-[#40bfff] via-[#66C6F3] to-[#00ccff] bg-clip-text text-transparent">A CRM dashboard for engineering teams</div>
           
-          <div className="mt-8 mx-4 px-4  md:mx-2 text-[#94A3B8] text-center text-bold md:text-xl text-[16px] ">
+          <div className="mt-8 px-4 mx-4  md:mx-2 text-[#94A3B8] text-center text-bold md:text-xl text-[16px] ">
             Boost engineering team’s productivity with Ocean CRM dashboard that streamlines<br></br>project management, collaboration, and data-driven decision-making.
-      </div>
+          </div>
       <Splashbtns/>
-     
-      
+
+      <div className="splash-img mt-1 flex justify-center mt-8 px-3">
+      <img 
+        src="/images/Hero-image.svg" 
+        className="w-11/12 sm:w-2/3 md:w-2/3 lg:w-[700px] xl:w-[650px] 2xl:w-[1000px] max-w-full h-auto mx-auto"
+        alt="Hero Image"
+        />
+      </div>
+
+
       </div>
     </div>
   );
--- compenents/navbar.tsx
+++ compenents/navbar.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Link from "next/link";
 import { Menu } from "lucide-react";
 
+
+
 export default function Navbar() {
 
   const [isOpen, setIsOpen] = useState(false);
@@ -13,22 +15,21 @@ export default function Navbar() {
       <div className="container mx-auto flex justify-between items-center">
 
       <Link href={"/"}>
-      <div className="flex items-center bg-[#0E1627] p-4 rounded-lg">
-      {/* Logo Icon */}
-      <div className="w-8 h-8 flex items-center justify-center mr-2">
-        <img src="/favicon.ico" alt="Logo Image" className="w-full h-full" />
-      </div>
-  
-      {/* Brand Name + Dot (inline) */}
-      <span className="text-white text-xl font-bold flex items-center">
-        Ocean<span className="text-[#0369A1]  mt-2 md:mt-0 text-3xl">•</span>
-      </span>
-    </div>
+          <div className="flex items-center bg-[#0E1627] p-4 rounded-lg">
+           { /* Logo Icon */}
+          <div className="w-8 h-8 flex items-center justify-center mr-2">
+              <img src="/favicon.ico" alt="Logo Image" className="w-full h-full" />
+          </div>
+          {/* Brand Name + Dot (inline) */}
+          <span className="text-white text-xl font-bold flex items-center">
+              Ocean<span className="text-[#0369A1]  mt-2 md:mt-0 text-3xl">•</span>
+          </span>
+          </div>
       </Link>
 
 
-        {/* Desktop Menu */}
-        <ul className="hidden md:flex space-x-8 font-semibold text-[#E1E7EF]">
+      {/* Desktop Menu */}
+      <ul className="hidden md:flex space-x-8 font-semibold text-[#E1E7EF]">
           {["Features","Pricing","Testimonials","Contact"].map((item) => (
             <li key={item}>
               <Link href={`#${item.toLowerCase()}`} className="hover:text-blue-400 transition">
@@ -38,10 +39,10 @@ export default function Navbar() {
           ))}
         </ul>
 
-        {/* Mobile Menu Button */}
-        <button onClick={() => setIsOpen(!isOpen)} className="md:hidden px-6">
-          <Menu className="w-6 h-6 text-[#E1E7EF]" />
-        </button>
+      {/* Mobile Menu Button */}
+      <button onClick={() => setIsOpen(!isOpen)} className="md:hidden px-6">
+        <Menu className="w-6 h-6 text-[#E1E7EF]" />
+      </button>
       </div>
 
       {/* Mobile Menu */}
