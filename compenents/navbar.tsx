0ba8230b1ee1e6c125b1f0ac16299591b6ec913e
osmanramadan <o@o.com>
2025-03-15 02:45:55 +0200

    'splash'

---
+++ compenents/navbar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { Menu } from "lucide-react";
+
+export default function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-[#0D1321] text-white py-1 px-9 bg-red-100">
+
+      <div className="container mx-auto flex justify-between items-center">
+
+    <div className="flex items-center bg-[#0E1627] p-4 rounded-lg">
+      {/* Logo Icon */}
+      <div className="w-8 h-8 flex items-center justify-center mr-2">
+        <img src="/favicon.ico" alt="Logo Image" className="w-full h-full" />
+      </div>
+  
+      {/* Brand Name + Dot (inline) */}
+      <span className="text-white text-xl font-bold flex items-center">
+        Ocean<span className="text-[#0369A1] text-3xl">•</span>
+      </span>
+    </div>
+
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-8 font-semibold text-[#E1E7EF]">
+          {["Features","Pricing","Testimonials","Contact"].map((item) => (
+            <li key={item}>
+              <Link href={`#${item.toLowerCase()}`} className="hover:text-blue-400 transition">
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile Menu Button */}
+        <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
+          <Menu className="w-6 h-6 text-[#E1E7EF]" />
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <ul className="md:hidden flex flex-col font-semibold items-center  mt-4 space-y-4 bg-[#0D1321] py-4">
+          {["Features", "Pricing", "Testimonials", "Contact"].map((item) => (
+            <li key={item}>
+              <Link href={`#${item.toLowerCase()}`} className="hover:text-blue-400 transition" onClick={() => setIsOpen(false)}>
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </nav>
+  );
+}
---
+++ compenents/splashbtns.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+export default function Splashbtns() {
+  return (
+    <div className="flex space-x-4 flex items-center justify-center mt-7">
+      {/* Primary Button */}
+      <button className="w-45 h-11 !bg-[#0284C7] hover:bg-[#0EA5E9] text-xl text-white font-medium py-1 px-1 rounded-md transition-all duration-300">
+        Get Started <span className="!ml-1 font-bold  !bg-[#0284C7]">→</span>
+      </button>
+
+      {/* Secondary Button */}
+      <button className="w-38  h-11 text-xl text-white py-2 px-3 rounded-md border-[0.1px] border-[#1E293B] hover:bg-[#1E293B] transition-all duration-300">
+        View Pricing
+      </button>
+    </div>
+  );
+}
