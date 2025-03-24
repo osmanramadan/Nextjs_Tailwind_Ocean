d551efba977b6114d092f85f6c9439082483c53c
osmanramadan <o@o.com>
2025-03-24 02:16:21 +0200

    'section-sys-meg'

--- app/page.tsx
+++ app/page.tsx
@@ -79,10 +79,30 @@ export default function Home() {
             </div>
             
 
-            </div>
+        </div>
 
       </div>
 
+
+      <div className=" grid grid-cols-1 lg:grid-cols-2 sm:mx-2 md:px-15 px-3 mx-5 text-center lg:text-start lg:mt-30 mt-15">
+      <div className="splash-img  flex justify-center mt-10 lg:pr-13">
+      <img 
+        src="/images/msg-sys.svg" 
+        className="w-11/12 sm:w-2/3 md:w-2/3 lg:w-[400px] xl:w-[600px] 2xl:w-[800px] max-w-full h-auto mx-auto"
+        alt="Hero Image"
+        />
+      </div>
+            <div className="py-22">
+              <div className="text-3xl sm:text-4xl md:text-5xl  text-[#3CB7EF] font-bold bg-gradient-to-r from-[#40bfff] via-[#2198FE] to-[#1CAAEB] bg-clip-text text-transparent">Integrated Messaging<br className="hidden lg:block"></br> System</div>
+              <div className="mt-5 text-[#94A3B8] text-center lg:text-start text-bold md:text-xl text-[18px] ">
+                The integrated messaging system within the CRM dashboard for engineering teams allows for instant and seamless communication among team members. It eliminates the need for external messaging platforms, keeping all project-related conversations organized and easily accessible within the dashboard's interface.         
+              </div>
+
+              </div>
+            <div>
+
+        </div>
+        </div>
 
       </div>
     </div>
